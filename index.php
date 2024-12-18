858c041c3f68eee881cc60f10739b5ecad5195e9
Davi Tostes <davisiqueira591@gmail.com>
2024-12-18 17:01:18 -0300

    feat: adding title

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/components/head.php' ?>
 
 <body class="bg-dark text-white d-flex flex-column vh-100">
-  <div class="container mt-5 flex-grow-1">
+  <div class="container flex-grow-1">
     <!-- Header -->
-    <div class="text-center mb-4">
+    <div class="text-center mt-5 mb-3">
+      <h4 class="text-light fw-bold">MediumBlue</h4>
       <p class="text-light">Manage your tasks effortlessly</p>
     </div>
 
