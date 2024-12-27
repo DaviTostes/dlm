50b7ed36efe4c391b6adef6135664d0edcdf3910
Davi Tostes <davisiqueira591@gmail.com>
2024-12-27 13:36:23 -0300

    feat: options in stats page

--- stats.php
+++ stats.php
@@ -11,6 +11,23 @@
       <p class="text-light">Manage your tasks effortlessly</p>
     </div>
 
+    <!-- Option Buttons -->
+    <div class="d-flex justify-content-end gap-2">
+      <!-- Stats -->
+      <div class="mb-3">
+        <button class="btn btn-primary p-2 rounded" hx-get="/" hx-target="body" hx-push-url="true">
+          <i class="bi bi-house"></i>
+        </button>
+      </div>
+
+      <!-- Sign out -->
+      <div class="mb-3">
+        <button class="btn btn-danger p-2 rounded" hx-post="/services/task/sign-out.php">
+          <i class="bi bi-box-arrow-left"></i>
+        </button>
+      </div>
+    </div>
+
     <!-- Charts Section -->
     <div class="row chart-container">
       <div class="col-md-6 mb-4">
